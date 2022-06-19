15a3912cc06d0a8c32f1b4b2e20d4d8bb2db5a0e
tarun2512 <77691995+tarun2512@users.noreply.github.com>
2022-06-19 22:28:20 +0530

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -238,10 +238,12 @@ class Results:
         # For X_train=np.reshape(no. of rows/samples, timesteps, no. of cols/features)
 
         # Building RNN
-        from keras.models import Sequential
-        from keras.layers import Dense
-        from keras.layers import Dropout
-        from keras.layers import LSTM
+        from tensorflow import keras
+        # from tensorflow.keras.models import models
+        from tensorflow.keras.models import Sequential
+        from tensorflow.keras.layers import Dense
+        from tensorflow.keras.layers import Dropout
+        from tensorflow.keras.layers import LSTM
 
         # Initialise RNN
         regressor = Sequential()
